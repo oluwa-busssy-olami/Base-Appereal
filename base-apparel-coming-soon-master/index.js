8eef04ec1d84fcba48913f414558e8194732d842
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-26 04:22:31 +0100

    the width

--- base-apparel-coming-soon-master/index.js
+++ base-apparel-coming-soon-master/index.js
@@ -11,6 +11,7 @@ input.addEventListener("input", (e) => {
     } else if (!/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(inputValue)) {
         error_message.textContent = "Enter a valid gmail";
         input.style.border = "1px solid red";
+        icon_error.classList.remove(hidden);
     } else {
         error_message.textContent = "";
         input.style.border = "";
