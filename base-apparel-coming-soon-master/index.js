a6017fe99d5612c2d2dc6742ba415e2050662f76
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-28 01:49:12 +0100

    minor change for mobile screen

--- base-apparel-coming-soon-master/index.js
+++ base-apparel-coming-soon-master/index.js
@@ -4,28 +4,30 @@ const error_message = document.querySelector(".error-message");
 const input = document.querySelector(".input");
 
 input.addEventListener("input", (e) => {
-    const inputValue = input.value.trim();
-    if (inputValue === "") {
-        error_message.textContent = "Input cant be blank";
-        input.style.border = "1px solid red";
-    } else if (!/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(inputValue)) {
-        error_message.textContent = "Enter a valid gmail";
-        input.style.border = "1px solid red";
-        icon_error.classList.remove(hidden);
-    } else {
-        error_message.textContent = "";
-        input.style.border = "";
-    }
+  const inputValue = input.value.trim();
+  if (inputValue === "") {
+    error_message.textContent = "Input cant be blank";
+    icon_error.style.display = "block";
+    input.style.border = "1px solid red";
+  } else if (!/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(inputValue)) {
+    error_message.textContent = "Enter a valid gmail";
+    input.style.border = "1px solid red";
+    icon_error.classList.remove(hidden);
+  } else {
+    error_message.textContent = "";
+    input.style.border = "";
+  }
 });
 button.addEventListener("click", (e) => {
-    e.preventDefault();
-    const inputValue = input.value.trim();
-    if (inputValue === "" || !/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(inputValue)) {
-        error_message.textContent = "Input cant be blank";
-        input.style.border = "1px solid red";
-        error_message.textContent = "Enter a valid gmail";
-        input.style.border = "1px solid red";
-    } else {
-        error_message.textContent = `Congratulations ${inputValue}`;
-    }
+  e.preventDefault();
+  const inputValue = input.value.trim();
+  if (inputValue === "" || !/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(inputValue)) {
+    error_message.textContent = "Input cant be blank";
+    input.style.border = "1px solid red";
+    icon_error.style.display = "block";
+    error_message.textContent = "Enter a valid gmail";
+    input.style.border = "1px solid red";
+  } else {
+    error_message.textContent = `Congratulations ${inputValue}`;
+  }
 });
