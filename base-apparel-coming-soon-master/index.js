90a6de72da5a4f29ffc60090c2e603466befe92e
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-30 09:10:51 +0100

    final design

--- base-apparel-coming-soon-master/index.js
+++ base-apparel-coming-soon-master/index.js
@@ -8,10 +8,10 @@ input.addEventListener("input", (e) => {
   if (inputValue === "") {
     error_message.textContent = "Input cant be blank";
     icon_error.style.display = "block";
-    input.style.border = "1px solid red";
+    input.style.border = "1px solid hsl(0, 93%, 68%)";
   } else if (!/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(inputValue)) {
     error_message.textContent = "Enter a valid gmail";
-    input.style.border = "1px solid red";
+    input.style.border = "1px solid hsl(0, 93%, 68%)";
     icon_error.style.display = "block";
   } else {
     error_message.textContent = "";
@@ -24,11 +24,12 @@ button.addEventListener("click", (e) => {
   const inputValue = input.value.trim();
   if (inputValue === "" || !/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(inputValue)) {
     error_message.textContent = "Input cant be blank";
-    input.style.border = "1px solid red";
+    input.style.border = "1px solid hsl(0, 93%, 68%)";
     icon_error.style.display = "block";
     error_message.textContent = "Enter a valid gmail";
-    input.style.border = "1px solid red";
+    input.style.border = "1px solid hsl(0, 93%, 68%)";
   } else {
     error_message.textContent = `Thank you, ${inputValue}! We'll keep you updated.`;
   }
+  input.value = "";
 });
